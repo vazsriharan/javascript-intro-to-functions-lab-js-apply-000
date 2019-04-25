34a93731e7ee3a0a0657e8e9119bc65df3fa3af3
Vaz Sriharan <info@vazsriharan.com>
2019-04-25 09:16:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   lowercase = string.toUpperCase()
-  if (string.toLowerCase === string){
+  if (string.toLowerCase() === string){
     return 'I can\'t hear you!';
   }
-  if (string.toUpperCase) === string){
+  if (string.toUpperCase() === string){
     return 'YES INDEED!';
   }
-  if (string = 'I love you Grandma' 
-  }
+  if (string = 'I love you Grandma'){
+    return 'I love you too';
   }
 }
 
